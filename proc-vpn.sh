1bc37fb1f13bb9b56c9122677f57ee6df4df652c
tuxvador <tuxvador@localhost.localdomain>
2024-09-09 16:21:21 +0200

    modified readme

--- proc-vpn.sh
+++ proc-vpn.sh
@@ -3,12 +3,6 @@
 # This script sets up a network namespace for a VPN connection and routes a command's traffic through it.
 # It ensures proper cleanup in case of script termination.
 
-# Check if the script is run as root
-if [ "$EUID" -ne 0 ]; then
-  echo "Please run as root"
-  exit 1
-fi
-
 # Variables
 INTF="wlan0"                             # Physical network interface to use
 NAMESPACE="vpn"                          # Name of the network namespace
@@ -17,7 +11,7 @@ VETH_NS="veth1"                          # Namespace end of the virtual ethernet
 IP_MAIN="10.2.1.1/24"                    # IP address for the main end of the veth pair
 IP_NS="10.2.1.2/24"                      # IP address for the namespace end of the veth pair
 NETWORK="10.2.1.0/24"                    # Network address for the veth pair
-VPN_CONFIG="/home/tuxvador/openvpn.ovpn" # Path to the VPN configuration file
+VPN_CONFIG="/home/tuxvador/.VPN/private_openvpn/openvpn.ovpn" # Path to the VPN configuration file
 COMMAND_TO_RUN="$@"                      # Command to run through the VPN passed as parameter to the script
 
 # Cleanup function to be called on exit
@@ -25,18 +19,18 @@ cleanup() {
   echo "Cleaning up..."
   
   # Kill the VPN process running in the namespace
-  pkill -f "ip netns exec $NAMESPACE openvpn"
+  sudo pkill -f "ip netns exec $NAMESPACE openvpn"
   
   # Delete the network namespace
-  ip netns delete $NAMESPACE
+  sudo ip netns delete $NAMESPACE
   
   # Remove the VETH pair
-  ip link delete $VETH_MAIN
+  sudo ip link delete $VETH_MAIN
   
   # Remove iptables rules related to this setup
-  iptables -D FORWARD -i ${INTF} -o ${VETH_MAIN} -j ACCEPT
-  iptables -D FORWARD -o ${INTF} -i ${VETH_MAIN} -j ACCEPT
-  iptables -t nat -D POSTROUTING -s ${NETWORK} -o ${INTF} -j MASQUERADE
+  sudo iptables -D FORWARD -i ${INTF} -o ${VETH_MAIN} -j ACCEPT
+  sudo iptables -D FORWARD -o ${INTF} -i ${VETH_MAIN} -j ACCEPT
+  sudo iptables -t nat -D POSTROUTING -s ${NETWORK} -o ${INTF} -j MASQUERADE
   
   echo "Cleanup complete."
 }
@@ -45,46 +39,46 @@ cleanup() {
 trap cleanup EXIT
 
 # Create a new network namespace
-ip netns add $NAMESPACE
+sudo ip netns add $NAMESPACE
 
 # Create a virtual ethernet pair for communication between the namespace and the host
-ip link add $VETH_MAIN type veth peer name $VETH_NS
+sudo ip link add $VETH_MAIN type veth peer name $VETH_NS
 
 # Assign one end of the veth pair to the namespace
-ip link set $VETH_NS netns $NAMESPACE
+sudo ip link set $VETH_NS netns $NAMESPACE
 
 # Configure the host side of the veth pair
-ip addr add $IP_MAIN dev $VETH_MAIN
-ip link set $VETH_MAIN up
+sudo ip addr add $IP_MAIN dev $VETH_MAIN
+sudo ip link set $VETH_MAIN up
 
 # Configure the namespace side of the veth pair
-ip netns exec $NAMESPACE ip addr add $IP_NS dev $VETH_NS
-ip netns exec $NAMESPACE ip link set $VETH_NS up
-ip netns exec $NAMESPACE ip link set lo up
+sudo ip netns exec $NAMESPACE ip addr add $IP_NS dev $VETH_NS
+sudo ip netns exec $NAMESPACE ip link set $VETH_NS up
+sudo ip netns exec $NAMESPACE ip link set lo up
 
 # Set up routing within the namespace, making the host side of the veth pair the default gateway
-ip netns exec $NAMESPACE ip route add default via ${IP_MAIN%/*}
+sudo ip netns exec $NAMESPACE ip route add default via ${IP_MAIN%/*}
 
 # Flush existing FORWARD and NAT rules, set policy to DROP by default for security
-iptables -P FORWARD DROP
-iptables -F FORWARD
-iptables -t nat -F
+sudo iptables -P FORWARD DROP
+sudo iptables -F FORWARD
+sudo iptables -t nat -F
 
 # Enable NAT (masquerading) for the namespace traffic
-iptables -t nat -A POSTROUTING -s ${NETWORK} -o ${INTF} -j MASQUERADE
+sudo iptables -t nat -A POSTROUTING -s ${NETWORK} -o ${INTF} -j MASQUERADE
 
 # Allow traffic forwarding between the physical interface and the veth pair
-iptables -A FORWARD -i ${INTF} -o ${VETH_MAIN} -j ACCEPT
-iptables -A FORWARD -o ${INTF} -i ${VETH_MAIN} -j ACCEPT
+sudo iptables -A FORWARD -i ${INTF} -o ${VETH_MAIN} -j ACCEPT
+sudo iptables -A FORWARD -o ${INTF} -i ${VETH_MAIN} -j ACCEPT
 
 # Allow all outgoing traffic from the host
-iptables -P OUTPUT ACCEPT
+sudo iptables -P OUTPUT ACCEPT
 
 # Start the VPN client inside the namespace
-ip netns exec $NAMESPACE openvpn --config $VPN_CONFIG &
+sudo ip netns exec $NAMESPACE openvpn --config $VPN_CONFIG &
 
 # Wait for the VPN connection to establish
 sleep 5
 
 # Execute the specified command within the network namespace
-ip netns exec $NAMESPACE $COMMAND_TO_RUN
+sudo ip netns exec $NAMESPACE $COMMAND_TO_RUN
